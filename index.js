615b9b1c795360f3369c316b0a95ffcd8c477172
myatt <rcoins84@yahoo.com>
2026-04-20 18:46:13 -0400

    colors kind of work

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
-document.addEventListener('DOMContentLoaded', adjustFontSize);
-document.addEventListener('DOMContentLoaded', toggleMode);
+class EditMode {
+    populate() {
+        let spans = document.querySelectorAll('.cell span');
+        spans.forEach(span => {
+            let word = span.textContent;
+            let input = document.createElement('input');
+            input.type = 'text';
+            input.value = word;
+            span.parentElement.appendChild(input);
+            span.remove();
+        });
+        document.querySelectorAll('.cell').forEach(cell => cell.removeEventListener('click', clickCell));
+    }
+
+    clickCell(cell) {
+
+    }
+}
+
+class PlayMode {
+
+    constructor() {
+        this.colors = [];
+        document.querySelectorAll('.color-button').forEach(button => this.colors.push(button.id));
+    }
+
+    populate() {
+        let inputs = document.querySelectorAll('.cell input');
+        inputs.forEach(input => {
+            let word = input.value.toUpperCase().trim();
+            let span = document.createElement('span');
+            span.textContent = word;
+            input.parentElement.appendChild(span);
+            input.remove();
+        });
+        document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', clickCell));
+    }
+
+}
+
+let mode, playMode, editMode;
+let selectedColor = null;
+
+
+document.addEventListener('DOMContentLoaded', init);
+
+
+function init() {
+    adjustFontSize();
+    toggleMode();
+    colorButtons();
+}
 
 function adjustFontSize() {
     let cells = document.querySelectorAll('.cell');
@@ -15,36 +65,46 @@ function adjustFontSize() {
 }
 
 function toggleMode() {
-    document.getElementById('mode').addEventListener('click', e => {
+    playMode = new PlayMode();
+    editMode = new EditMode();
+
+    let modeButton = document.getElementById('mode')
+    modeButton.addEventListener('click', e => {
         if (e.target.innerText === 'Enter Words') {
             e.target.innerText = 'Play';
-            editWords();
+            mode = editMode;
         } else {
             e.target.innerText = 'Enter Words';
-            readOnlyWords();
+            mode = playMode;
         }
+        mode.populate();
     });
 }
 
-function readOnlyWords() {
-    let inputs = document.querySelectorAll('.cell input');
-    inputs.forEach(input => {
-        let word = input.value.toUpperCase().trim();
-        let span = document.createElement('span');
-        span.textContent = word;
-        input.parentElement.appendChild(span);
-        input.remove();
+function colorButtons() {
+    let buttons = document.querySelectorAll('.color-button');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            buttons.forEach(b => b.classList.remove('color-selected'));
+            button.classList.add('color-selected');
+            selectedColor = button.id;
+        });
     });
 }
 
-function editWords() {
-    let spans = document.querySelectorAll('.cell span');
-    spans.forEach(span => {
-        let word = span.textContent;
-        let input = document.createElement('input');
-        input.type = 'text';
-        input.value = word;
-        span.parentElement.appendChild(input);
-        span.remove();
-    });
+function clickCell(e) {
+    let cell = e.currentTarget;
+    if (cell.classList.contains(selectedColor)) {
+        cell.classList.remove(selectedColor);
+        cell.classList.add('ungrouped');
+        return;
+    } else if (cell.classList.contains('ungrouped')) {
+        cell.classList.remove('ungrouped');
+        cell.classList.add(selectedColor);
+        return;
+    } else {
+        cell.classList.remove(...mode.colors);
+        cell.classList.add(selectedColor);
+        return;
+    }
 }
